96f1d8276dad1a4e9aae253d157f50a99d0b6f78
vivekchourasiya <vivek.chourasiya@culturemachine.in>
2016-07-02 18:10:31 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello');
+  res.send('Hello my boy');
 });
 
 app.listen(3000, function () {
